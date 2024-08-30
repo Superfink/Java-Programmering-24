8a8d512c1e3059a4e32b458221423539e522fa61
Superfink <kristin.kullberg@gmail.com>
2024-08-30 14:11:44 +0200

    Update JavaPlayground.java

--- Coding_playground/Kristin/JavaPlayground.java
+++ Coding_playground/Kristin/JavaPlayground.java
@@ -1,6 +1,6 @@
 public class JavaPlayground {
    public static void main(String[] args){
-    System.out.println("Happy days!");
+    System.out.println("Happy days are here again!");
    }    
 }
 
