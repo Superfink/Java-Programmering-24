a1d288b32abb3c91f893be45de2854b353462430
Kristin Kullberg <kristin.kullberg@gmail.com>
2024-08-30 14:01:46 +0200

    Im changing

--- Coding_playground/Kristin/JavaPlayground.java
+++ Coding_playground/Kristin/JavaPlayground.java
@@ -1,5 +1,7 @@
-public class Main(){
-   public static void mjau(String[] args){
+public class JavaPlayground {
+   public static void main(String[] args){
     System.out.println("Happy days!");
    }    
 }
+
+
